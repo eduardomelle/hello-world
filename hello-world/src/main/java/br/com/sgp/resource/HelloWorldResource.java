e2309cfe165286017e9be0d0c3faf4b8e2892695
eduardo <eduardomelle@gmail.com>
2020-05-15 23:41:02 -0300

    First commit.

---
+++ hello-world/src/main/java/br/com/sgp/resource/HelloWorldResource.java
@@ -0,0 +1,14 @@
+package br.com.sgp.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldResource {
+
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello World!!!";
+	}
+
+}
